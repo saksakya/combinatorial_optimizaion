a0b58f44f74472b6a07f4f258a718f1417ccbde2
saksakya <saksakya@gmail.com>
2023-07-05 20:03:38 +0900

    final

--- model/conbination.py
+++ model/conbination.py
@@ -14,6 +14,8 @@ class find_combination(object):
         self.group_list = [self.num_to_alpha(g + 1) for g in range(self.group_nums)]
         # 個人とグループの全組み合わせ
         self.pariticipant_group = [(i,g) for i, s in enumerate(self.status) for g in self.group_list]
+        # 個人と個人の全組み合わせ
+        self.pair= [(i,j) for i, s in enumerate(self.status) for j, k in enumerate(self.status)]
         # print(self.pariticipant_group)
         
     def num_to_alpha(self, num):
@@ -30,19 +32,17 @@ class find_combination(object):
 
         # 変数の定義 (Memo binary:0or1)
         x = pp.LpVariable.dicts('x', self.pariticipant_group, cat = 'Binary')
+        y = pp.LpVariable.dicts('y', self.pair, cat = 'Binary')
 
         #print(x)
+        #print(y)
         
         # 目的条件(回数の総和ができる限り少なくなるようにする)
-        objective = 0
-        for g in self.group_list:
-            for i in range(self.n):
-                for j in range(self.n):
-                    if i != j:
-                        objective += pp.lpSum(self.number_list[i][j] * x[j, g]) 
+        objective = pp.lpSum(self.number_list[i][j] * y[i, j] for i in range(self.n) for j in range(self.n) if i!= j )
                         
         problem += objective
-
+        #print(problem)
+ 
         # 制約条件1(参加者は、１つのグループに割り当て)
         for i, s in enumerate(self.status):
             if s == True:
@@ -52,7 +52,14 @@ class find_combination(object):
         for g in self.group_list:
             problem += pp.lpSum([x[i, g] for i, s in enumerate(self.status) if s == True]) >= self.min_member
             problem += pp.lpSum([x[i, g] for i, s in enumerate(self.status) if s == True]) <= self.max_member
-
+            
+        # 制約条件3(変数xと変数yの関連付け、同じグループならyが１になる。)           
+        for g in self.group_list:
+            for i in  range(self.n):
+                for j in range(self.n):
+                    if i != j:
+                        problem += x[i, g] + x[j, g] -1 <= y[i, j]
+        
         #print(problem)
         
         # 最適化の実行
